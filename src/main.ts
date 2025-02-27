d4b374a406632e90410bcba679061d1ed3eb4786
Álvaro Henrique Nunes de Lima <alvarolima@weg.net>
2025-02-27 16:59:03 -0300

    chore: format main

--- src/main.ts
+++ src/main.ts
@@ -6,39 +6,39 @@ import { AppModule } from './app.module';
 const cors = require('cors');
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    // Insert Here
+  // Insert Here
+  app.setGlobalPrefix('/api');
 
-    app.setGlobalPrefix('/api');
+  app.use(cors());
 
-    app.use(cors());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
-    app.useGlobalPipes(new ValidationPipe({
-        transform: true,
-    }));
+  const config = new DocumentBuilder()
+    .setTitle('Pendencies')
+    .setDescription('App for pendencies management.')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    .build();
 
-    const config = new DocumentBuilder()
-        .setTitle('Pendencies')
-        .setDescription('App for pendencies management.')
-        .setVersion('1.0.0')
-        .addBearerAuth()
-        .addSecurityRequirements('bearer')
-        .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [],
+  });
 
-    const document = SwaggerModule.createDocument(app, config, {
-        extraModels: [],
-    });
+  const CustomOptions = {
+    customSiteTitle: 'Pendencies',
+  };
 
-    const CustomOptions = {
-        customSiteTitle: 'Pendencies',
-    }
+  SwaggerModule.setup('api', app, document, CustomOptions);
 
-    SwaggerModule.setup('api', app, document, CustomOptions);
-
-    // Insert Here
-
-    await app.listen(process.env.PORT ?? 3000);
+  // Insert Here
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
